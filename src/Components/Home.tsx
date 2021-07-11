28f1682f83ec850657f722d195c6925abd5a1269
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2021-07-11 22:20:27 +0530

    implemented search function in Home page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -17,44 +17,87 @@ const gridstyle: any = { xs: 2.2, justifyContent: "center" }
 function Home() {
     const [loading, setLoading] = useState(false)
     const [books, setBooks] = useState([]);
+    const [sBooks, setSBooks] = useState<any>();
+    const [sname, setSName] = useState<any>();
+    var bookDetails: any;
     useEffect(() => {
         setLoading(true)
         var data = getAllBooks();
         data.then((resp: any) => { console.log(resp); setBooks(resp.data); setLoading(false) })
     }, [])
+
+    const search = (name: any) => {
+        var n: any;
+        var e: any;
+        books.map((book: any, i: any) => {
+            if (name == book.title) {
+                n = book;
+            }
+            else {
+                e = "All";
+            }
+        })
+        console.log(n, e)
+        return n;
+
+    }
+
     return (
         <>
             {
                 loading ? <div>Loading ...</div> :
                     <div>
-                        <Grid container direction="row"
-                            justifyContent="flex-start"
-                            alignItems="center"
-                            spacing={3}
-                            style={{ margin: "10px" }}
-                        >
-                            {
-                                books.map((book: any, i: any) => (
-                                    <Grid style={gridstyle}>
-                                        <Link to={{ pathname: "/Details", state: { id: book.id } }}>
-                                            <Card variant="outlined" style={{ margin: "15px", background: 'rgb(205, 204, 212)' }}>
-                                                <CardContent>
-                                                    <Typography style={{ margin: "5px" }}>
-                                                        <img src={book.imageLink} width="300" height="100" />
-                                                    </Typography>
-                                                    <Typography >
-                                                        <p style={{ fontSize: "10px" }}>{book.title}</p>
-                                                    </Typography>
-                                                    <Typography >
-                                                        <p style={{ fontSize: "8px" }}>{book.author}</p>
-                                                    </Typography>
-                                                </CardContent>
-                                            </Card>
-                                        </Link>
-                                    </Grid>
-                                ))
-                            }
-                        </Grid>
+
+                        Search<input type='text' onChange={(e) => { setSName(e.target.value); bookDetails = search(e.target.value); setSBooks(bookDetails) }} />
+
+                        {sBooks == null || sBooks == undefined ?
+                            <Grid container direction="row"
+                                justifyContent="flex-start"
+                                alignItems="center"
+                                spacing={3}
+                                style={{ margin: "10px" }}
+                            >
+                                {
+                                    books.map((book: any, i: any) => (
+                                        <Grid style={gridstyle}>
+                                            <Link to={{ pathname: "/Details", state: { id: book.id } }}>
+                                                <Card variant="outlined" style={{ margin: "15px", background: 'rgb(205, 204, 212)' }}>
+                                                    <CardContent>
+                                                        <Typography style={{ margin: "5px" }}>
+                                                            <img src={book.imageLink} width="300" height="100" />
+                                                        </Typography>
+                                                        <Typography >
+                                                            <p style={{ fontSize: "10px" }}>{book.title}</p>
+                                                        </Typography>
+                                                        <Typography >
+                                                            <p style={{ fontSize: "8px" }}>{book.author}</p>
+                                                        </Typography>
+                                                    </CardContent>
+                                                </Card>
+                                            </Link>
+                                        </Grid>
+                                    ))
+                                }
+                            </Grid> : <div>
+                                <Grid style={gridstyle}>
+                                    <Link to={{ pathname: "/Details", state: { id: sBooks.id } }}>
+                                        <Card variant="outlined" style={{ margin: "15px", background: 'rgb(205, 204, 212)' }}>
+                                            <CardContent>
+                                                <Typography style={{ margin: "5px" }}>
+                                                    <img src={sBooks.imageLink} width="300" height="100" />
+                                                </Typography>
+                                                <Typography >
+                                                    <p style={{ fontSize: "10px" }}>{sBooks.title}</p>
+                                                </Typography>
+                                                <Typography >
+                                                    <p style={{ fontSize: "8px" }}>{sBooks.author}</p>
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Link>
+                                </Grid>
+                            </div>
+                        }
                     </div>
             }
         </>
