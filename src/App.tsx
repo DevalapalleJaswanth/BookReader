79287951101563c8b2771870c2fb12626fce4ad3
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2021-07-11 12:52:06 +0530

    modified App.tsx and got the data from the api, stored it in local state variable

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import {
   Link,
   Switch
 } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getAllBooks } from './Services/Services';
 import Home from './Components/Home';
 import Details from './Components/Details';
 function App() {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    var data = getAllBooks();
+    data.then((resp: any) => { console.log(resp); setBooks(resp.data); })
+  }, [])
   return (
     <>
+      {console.log(books)}
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
